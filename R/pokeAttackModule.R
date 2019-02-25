991967319bdddb29c9c7e863dd86c860a631ff7e
Divad0175 <dgranjon@ymail.com>
2019-02-26 00:04:21 +0100

    more elements in attacks

--- R/pokeAttackModule.R
+++ R/pokeAttackModule.R
@@ -6,7 +6,22 @@
 #' @export
 pokeAttackUi <- function(id) {
   ns <- shiny::NS(id)
-  uiOutput(ns("poke_attack"))
+  tagList(
+    fluidRow(
+      column(
+        width = 12,
+        align = "center",
+        multiInput(
+          inputId = ns("pokeAttackSelect"),
+          label = "Select pokemon abilities:",
+          choices = names(pokeAttacks),
+          selected = names(pokeAttacks)[c(1:10)],
+          width = "350px"
+        )
+      )
+    ),
+    uiOutput(ns("poke_attack"))
+  )
 }
 
 
@@ -22,18 +37,46 @@ pokeAttack <- function(input, output, session, attacks) {
   ns <- session$ns
 
   output$poke_attack <- renderUI({
-    lapply(seq_along(attacks), function(i) {
-      tablerBlogCard(
-        title = attacks[[i]]$name,
-        author = attacks[[i]]$type$name,
-        date = attacks[[i]]$target$name,
-        href = NULL,
-        src = NULL,
-        avatarUrl = NULL,
-        width = 6,
-        attacks[[i]]$flavor_text_entries$flavor_text[44]
-      )
-    })
+
+    req(input$pokeAttackSelect)
+
+    fluidRow(
+      lapply(seq_along(input$pokeAttackSelect), function(i) {
+
+        selected <- input$pokeAttackSelect[[i]]
+
+        tablerBlogCard(
+          title = attacks[[selected]]$name,
+          author = attacks[[selected]]$type$name,
+          date = attacks[[selected]]$target$name,
+          href = NULL,
+          src = NULL,
+          avatarUrl = NULL,
+          width = 6,
+          tablerTable(
+            title = "Main Stats",
+            width = 4,
+            tablerTableItem(
+              right = "Power",
+              left = attacks[[selected]]$power
+            ),
+            tablerTableItem(
+              right = "PP",
+              left = attacks[[selected]]$pp
+            ),
+            tablerTableItem(
+              right = "Accuracy",
+              left = attacks[[selected]]$accuracy
+            ),
+            tablerTableItem(
+              right = "Priority",
+              left = attacks[[selected]]$priority
+            )
+          ),
+          attacks[[i]]$flavor_text_entries$flavor_text[44]
+        )
+      })
+    )
   })
 
 
