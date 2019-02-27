b21638344e629e37ccf332e030616d40fc1ee644
Divad0175 <dgranjon@ymail.com>
2019-02-27 11:25:31 +0100

    add the network

--- R/pokeNetworkModule.R
+++ R/pokeNetworkModule.R
@@ -14,10 +14,9 @@ pokeNetworkUi <- function(id) {
       id = ns("myPushbar"),
 
       # content
-      h4("HELLO"),
-      actionButton(ns("close"), "Close pushbar")
-    )#,
-    #uiOutput(ns("poke_network"))
+      h4("Hi")
+    ),
+    visNetworkOutput(ns("pokeNet"))
   )
 }
 
@@ -30,15 +29,74 @@ pokeNetworkUi <- function(id) {
 #' @param attacks Data containing all pokemon abilities.
 #' @export
 pokeNetwork <- function(input, output, session, mainData) {
-  setup_pushbar(session, blur = TRUE) # setup
 
   ns <- session$ns
+
+  #-------------------------------------------------------------------------
+  # Pushbar setup, events, ...
+  #-------------------------------------------------------------------------
+  setup_pushbar(session, blur = TRUE) # setup
 
   observeEvent(input$open, {
     pushbar_open(session, id = ns("myPushbar"))
   })
 
-  observeEvent(input$close, {
-    pushbar_close(session)
+  #-------------------------------------------------------------------------
+  # Network: nodes, edges, events, ...
+  #-------------------------------------------------------------------------
+
+  pokeNames <- names(mainData)
+  sprites <- vapply(seq_along(pokeNames), FUN = function(i) mainData[[i]]$sprites$front_default, FUN.VALUE = character(1))
+
+  output$pokeNet <- renderVisNetwork({
+
+    nodes <- data.frame(
+      id = 1:length(mainData),
+      shape = rep("image", length(mainData)),
+      image = sprites,
+      label = pokeNames,
+      fixed = list("x" = FALSE, "y" = FALSE),
+      size = rep(100, length(mainData)),
+      #physics = rep(FALSE, 16)
+      hidden = rep(FALSE, length(mainData)),
+      stringsAsFactors = FALSE
+    )
+    edges <- NULL
+
+    visNetwork(nodes, edges, width = "100%") %>%
+      visEvents(selectNode = paste0("function(nodes) { Shiny.setInputValue('", ns("current_node_id"), "', nodes.nodes); }")) %>%
+      visEvents(deselectNode = paste0(
+        "function(nodes) {
+          Shiny.setInputValue('", ns("current_node_id"), "', 'null');
+          //Shiny.setInputValue('", ns("current_node_id_zoom"), "', 'null');
+         }
+        "
+        )
+      ) %>%
+      visNodes(
+        shapeProperties =
+          list(
+            useBorderWithImage = FALSE,
+            interpolation = FALSE
+          )
+      ) %>%
+      visOptions(
+        highlightNearest = FALSE,
+        clickToUse = FALSE,
+        manipulation = FALSE, # to manually add nodes and edges. Could be interesting ...
+        collapse = FALSE,
+        autoResize = TRUE
+      ) %>%
+      visInteraction(
+        hover = TRUE,
+        hoverConnectedEdges = FALSE,
+        selectConnectedEdges = FALSE,
+        multiselect = FALSE,
+        dragNodes = TRUE,
+        dragView = TRUE,
+        zoomView = TRUE,
+        navigationButtons = FALSE,
+        selectable = TRUE
+      )
   })
 }
